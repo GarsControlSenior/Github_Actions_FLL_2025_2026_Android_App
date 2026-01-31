4f537151d718ea4f829c075a1e89f0d246daa310
GarsControlSenior <159635035+GarsControlSenior@users.noreply.github.com>
2026-01-31 10:42:08 +0100

    test mit ble

--- main.py
+++ main.py
@@ -1,55 +1,31 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.graphics import Color, Rectangle
-from kivy.clock import Clock
-import math
+[app]
 
-class CompassScreen(Widget):
+title = CompassApp
+package.name = compassapp
+package.domain = test.compass
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+source.dir = .
+source.include_exts = py
 
-        # Hintergrund
-        with self.canvas.before:
-            Color(0, 0, 0.6, 1)  # Dunkelblau
-            self.bg = Rectangle(size=self.size, pos=self.pos)
+version = 0.1
 
-        self.bind(size=self.update_bg, pos=self.update_bg)
+requirements = python3,kivy,bleak
 
-        # Anzeige
-        self.label = Label(
-            text="NORD: 0°",
-            font_size="40sp",
-            color=(1, 1, 1, 1),
-            center=self.center
-        )
-        self.add_widget(self.label)
+orientation = portrait
+fullscreen = 0
 
-        self.angle = 0
-        Clock.schedule_interval(self.update_direction, 0.5)
+# Android BLE Berechtigungen
+android.permissions = BLUETOOTH,BLUETOOTH_ADMIN,ACCESS_FINE_LOCATION
 
-    def update_bg(self, *args):
-        self.bg.size = self.size
-        self.bg.pos = self.pos
-        self.label.center = self.center
+android.api = 32
+android.minapi = 21
+android.ndk_api = 21
 
-    def update_direction(self, dt):
-        # 🔁 SIMULATION (ersetzt später Arduino-Wert)
-        self.angle = (self.angle + 10) % 360
+android.archs = arm64-v8a, armeabi-v7a
+android.allow_backup = True
 
-        direction = self.get_direction(self.angle)
-        self.label.text = f"NORD: {self.angle}°\n{direction}"
+p4a.bootstrap = sdl2
 
-    def get_direction(self, angle):
-        dirs = ["N", "NO", "O", "SO", "S", "SW", "W", "NW"]
-        return dirs[int((angle + 22.5) / 45) % 8]
-
-
-class MainApp(App):
-    def build(self):
-        return CompassScreen()
-
-
-if __name__ == "__main__":
-    MainApp().run()
+[buildozer]
+log_level = 2
+warn_on_root = 1
