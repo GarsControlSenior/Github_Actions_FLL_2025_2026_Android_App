4dfb03c772b882bd5289281f8980b55ac2665bee
GarsControlSenior <159635035+GarsControlSenior@users.noreply.github.com>
2026-01-31 15:26:18 +0100

    neuer versuch

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
 from kivy.clock import Clock
-from jnius import autoclass, cast
+from jnius import autoclass, cast, PythonJavaClass, java_method
 from math import radians
 
 # Android BLE Klassen
 BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
 BluetoothManager = autoclass('android.bluetooth.BluetoothManager')
 PythonActivity = autoclass('org.kivy.android.PythonActivity')
+BluetoothGattCallback = autoclass('android.bluetooth.BluetoothGattCallback')
+BluetoothGattCharacteristic = autoclass('android.bluetooth.BluetoothGattCharacteristic')
+BluetoothGatt = autoclass('android.bluetooth.BluetoothGatt')
 UUID = autoclass('java.util.UUID')
 
 SERVICE_UUID = "0000180C-0000-1000-8000-00805f9b34fb"
@@ -24,6 +27,35 @@ class CompassWidget(Widget):
     def update_arrow(self, dt):
         self.arrow.angle = self.heading
 
+# BLE Callback Klasse
+class MyGattCallback(PythonJavaClass):
+    __javainterfaces__ = ['android/bluetooth/BluetoothGattCallback']
+    __javacontext__ = 'app'
+
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+
+    @java_method('(Landroid/bluetooth/BluetoothGatt;I)V')
+    def onConnectionStateChange(self, gatt, status, newState):
+        if newState == 2:  # Connected
+            gatt.discoverServices()
+
+    @java_method('(Landroid/bluetooth/BluetoothGatt;I)V')
+    def onServicesDiscovered(self, gatt, status):
+        service = gatt.getService(UUID.fromString(SERVICE_UUID))
+        char = service.getCharacteristic(UUID.fromString(CHAR_UUID))
+        gatt.setCharacteristicNotification(char, True)
+        # Descriptor für Notifications
+        descriptor = char.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"))
+        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE)
+        gatt.writeDescriptor(descriptor)
+
+    @java_method('(Landroid/bluetooth/BluetoothGatt;Landroid/bluetooth/BluetoothGattCharacteristic;)V')
+    def onCharacteristicChanged(self, gatt, characteristic):
+        value = characteristic.getFloatValue(0)  # Empfangenes Float
+        self.app.compass.heading = value
+
 class CompassApp(App):
     def build(self):
         self.compass = CompassWidget()
@@ -34,16 +66,24 @@ class CompassApp(App):
     def setup_ble(self, dt):
         activity = PythonActivity.mActivity
         service = activity.getSystemService(activity.BLUETOOTH_SERVICE)
-        adapter = cast('android.bluetooth.BluetoothManager', service).getAdapter()
+        manager = cast('android.bluetooth.BluetoothManager', service)
+        adapter = manager.getAdapter()
 
         if not adapter.isEnabled():
             adapter.enable()
 
-        # Scan starten (hier stark vereinfacht, für Test)
-        print("BLE Scan sollte starten... (Android BLE API nötig)")
+        paired_devices = adapter.getBondedDevices().toArray()
+        target_device = None
+        for d in paired_devices:
+            if "NanoCompass" in d.getName():  # Name vom Arduino
+                target_device = d
+                break
 
-        # Hier müsstest du mit pyjnius die Services und Characteristics auslesen
-        # und dann self.compass.heading = empfangener Winkel setzen
+        if target_device:
+            callback = MyGattCallback(self)
+            self.gatt = target_device.connectGatt(activity, False, callback)
+        else:
+            print("Arduino BLE Gerät nicht gefunden!")
 
 if __name__ == '__main__':
     CompassApp().run()
